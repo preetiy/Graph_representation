1e5fb58d5e92dc5fd9fe6b3e4741deff25f6af0b
PREETI YADAV <preetiy301998@gmail.com>
2018-10-03 01:22:02 +0530

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -80,7 +80,7 @@ def main():
     graph = Graph()
     graph.insert_edge(100, 1, 2)
     graph.insert_edge(101, 1, 3)
-    graph.insert_edge(102, 1, 4)
+    graph.insert_edge(102, 1, 3)
     graph.insert_edge(103, 3, 4)
 
     print(graph.EdgeList())
